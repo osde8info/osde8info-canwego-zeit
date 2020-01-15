4369fb27ec41ba70e699e0a41a6c4f9546624265
Clive Da <cliveda@example.com>
2020-01-15 18:45:41 +0000

    touched

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 app.get('*', (req, res) => {
     res.set('Content-Type', 'text/html')
     res.status(200).send(`
-        <h1>what can we do today</h1>
+        <h1>where can we go today</h1>
         <h2>visit <a href="https://www.visitsurrey.com/">surrey</a></h2>
         <h2>visit <a href="https://www.visitsurrey.com/explore/towns">surrey map</a></h2>
         <h2>visit <a href="https://www.visitsoutheastengland.com/places-to-visit/sussex">sussex</a></h2>
