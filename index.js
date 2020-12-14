63a068d355c3abb071739355b40827e598f02674
Clive D <osde8info@users.noreply.github.com>
2020-12-14 21:58:30 +0000

    Update index.js
    added whatson

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ app.get('*', (req, res) => {
     res.set('Content-Type', 'text/html')
     res.status(200).send(`
         <h1>where can we go today</h1>
-        <h2>visit <a href="https://www.visitsurrey.com/">surrey</a></h2>
-        <h2>visit <a href="https://www.visitsurrey.com/explore/towns">surrey map</a></h2>
-        <h2>visit <a href="https://www.visitsoutheastengland.com/places-to-visit/sussex">sussex</a></h2>
-        <h2>visit <a href="https://www.visitsoutheastengland.com/places-to-visit/sussex/map">sussex map</a></h2>
+
+        <h2>Surrey</h2>
+        <h3>visit <a href="https://www.visitsurrey.com/">surrey</a></h3>
+        <h3>visit <a href="https://www.visitsurrey.com/explore/towns">surrey map</a></h3>
+        <h3>visit <a href="https://www.visitsoutheastengland.com/events/whats-on-in-surrey">surrey whats on</a></h3>
+
+        <h2>Sussex</h2>
+        <h3>visit <a href="https://www.visitsoutheastengland.com/places-to-visit/sussex">sussex</a></h3>
+        <h3>visit <a href="https://www.visitsoutheastengland.com/places-to-visit/sussex/map">sussex map</a></h3>
+        <h3>visit <a href="https://www.visitsoutheastengland.com/events/whats-on-in-sussex">sussex whats on</a></h3>
     `)
 })
 
