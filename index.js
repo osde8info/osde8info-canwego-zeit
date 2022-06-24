f0c7963c8d2c26d76e892c4354d2244b6d973bb6
Clive Darra <osde8info@example.com>
2022-06-24 14:48:28 +0100

    m

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ app.get('*', (req, res) => {
     res.status(200).send(`
         <h1>where can we go today</h1>
         
+        <h2>Kent</h2>
+        <h3>visit <a href="https://www.visitkent.com/">kent</a></h3>
+        <h3>visit <a href="https://www.visitkent.com/explore/towns">kent map</a></h3>
+        <h3>visit <a href="https://www.visitsoutheastengland.com/events/whats-on-in-kent">kent whats on</a></h3>
+
         <h2>Surrey</h2>
         <h3>visit <a href="https://www.visitsurrey.com/">surrey</a></h3>
         <h3>visit <a href="https://www.visitsurrey.com/explore/towns">surrey map</a></h3>
